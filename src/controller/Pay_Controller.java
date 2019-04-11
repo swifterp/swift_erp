affa53806869fe720afbdfad5b8a6b016e89e42f
ksm505 <ksm505@DESKTOP-35B2GKG>
2019-04-12 01:10:17 +0900

    sdfas

--- src/controller/Pay_Controller.java
+++ src/controller/Pay_Controller.java
@@ -9,8 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import logic.pay.DeleteAllowanceItem;
+import logic.pay.DeleteDeductionItem;
+import logic.pay.DeletePayroll;
+import logic.pay.InsertAllowanceItem;
 import logic.pay.InsertDailyworkList;
+import logic.pay.InsertDeductionItem;
+import logic.pay.InsertPayrollList;
+import logic.pay.InsertPersonalCalcList;
+import logic.pay.InsertTotalCalcList;
 import logic.pay.InsertUnderyearList;
+import logic.pay.InsertWorkConfirm;
 import logic.pay.SelectAllowanceItem;
 import logic.pay.SelectDailyworkList;
 import logic.pay.SelectDeductionItem;
@@ -18,10 +26,43 @@ import logic.pay.SelectMonthlyempList;
 import logic.pay.SelectPersonalPayList;
 import logic.pay.SelectSalaryCalcList;
 import logic.pay.SelectSpecsList;
+import logic.pay.SelectWorkConfirm;
+import logic.pay.UpdatePayConfirmCancelList;
+import logic.pay.UpdatePayConfirmList;
 
 @Controller
-@RequestMapping("service/pay")
+@RequestMapping("/pay")
 public class Pay_Controller {
+
+	@Autowired
+	private SelectAllowanceItem sai;
+	
+	@RequestMapping("/allowance")
+	public String pay_Allowance(Model model, @RequestParam(value="classify", defaultValue="0") Integer classify) {
+		
+		model.addAttribute("list", sai.callAllowanceItemDao(classify));
+		
+		if(classify == 0) {
+			return "pay/pay_allowanceItem";
+		} else if(classify == 2) {
+			return "emp/emp_empAdd";
+		} else {
+			return "pay/pay_dailyworkInsert";
+		}
+	}
+	
+	@Autowired
+	private InsertAllowanceItem iai;
+	
+	@RequestMapping("/insertAllowance")
+	public String pay_insertAllowance(Model model, @RequestParam(value="name", defaultValue="0") String name,
+												   @RequestParam(value="division", defaultValue="0") String division) {
+		
+		model.addAttribute("list", iai.callInsertAllowanceDao(name, division));
+		
+		return "pay/pay_allowanceItem";
+		
+	}
 	
 	@Autowired
 	private DeleteAllowanceItem dai;
@@ -48,6 +89,30 @@ public class Pay_Controller {
 	}
 	
 	@Autowired
+	private InsertDeductionItem idi;
+	
+	@RequestMapping("/insertDeduction")
+	public String pay_insertDeduction(Model model, @RequestParam(value="name", defaultValue="0") String name) {
+		
+		model.addAttribute("list", idi.callInsertDeductionDao(name));
+		
+		return "pay/pay_deductionItem";
+		
+	}
+	
+	@Autowired
+	private DeleteDeductionItem ddi;
+	
+	@RequestMapping("/deleteDeduction")
+	public String pay_deleteDeduction(Model model, @RequestParam(value="deduction_no", defaultValue="0") Integer deduction_no) {
+				
+		model.addAttribute("list", ddi.callDeleteDeductionDao(deduction_no));
+		
+		return "pay/pay_deductionItem";
+		
+	}
+	
+	@Autowired
 	private SelectSalaryCalcList ssl;
 	
 	@RequestMapping("/payroll")
@@ -66,6 +131,7 @@ public class Pay_Controller {
 	public String pay_PersonalPay(Model model, @RequestParam(value="report", defaultValue="0") String report) {
 
 		model.addAttribute("list", spl.callPersonalPayDao(report));
+		model.addAttribute("deduc", spl.callPersonalDeducDao(report));
 		
 		return "pay/pay_personalPayList";
 
@@ -86,20 +152,24 @@ public class Pay_Controller {
 	@Autowired
 	private InsertUnderyearList iul;
 	
+	@RequestMapping("/underyearSearch")
+	public String pay_underyearSearch(Model model) {
+		
+		model.addAttribute("list", iul.callUnderyearDao());
+		
+		return "pay/pay_underyearSearch";
+		
+	}
+	
 	@RequestMapping("/underyear")
 	public String pay_underyear(Model model, @RequestParam HashMap<String, Integer> percentage) {
 		
 		model.addAttribute("list", iul.callUnderyearDao(percentage));
 		
-		return "pay/pay_underyearList";
+		return "pay/pay_underyearSearch";
 		
 	}
 
-	
-
-	
-	
-	
 	@Autowired
 	private SelectMonthlyempList sml;
 	
@@ -117,11 +187,13 @@ public class Pay_Controller {
 	private SelectDailyworkList sdl;
 	
 	@RequestMapping("/dailyworkList")
-	public String pay_Dailywork(Model model, @RequestParam(value="year", defaultValue="error") String year
-											, @RequestParam(value="month", defaultValue="error") String month
-											, @RequestParam(value="day", defaultValue="error") String day) {
+	public String pay_Dailywork(Model model, @RequestParam String selectedDate) {
+
+//		 @RequestParam(value="year", defaultValue="error") String year
+//			, @RequestParam(value="month", defaultValue="error") String month
+//			, @RequestParam(value="day", defaultValue="error") String day
 		
-		model.addAttribute("list", sdl.callDailyworkListDao(year, month, day));
+		model.addAttribute("list", sdl.callDailyworkListDao(selectedDate));
 		
 		return "pay/pay_dailyworkList";
 		
@@ -131,17 +203,113 @@ public class Pay_Controller {
 	private InsertDailyworkList idl;
 	
 	@RequestMapping("/dailyworkInsert")
-	public String pay_Dailywork(Model model, @RequestParam(value="year", defaultValue="error") String year
-											, @RequestParam(value="month", defaultValue="error") String month
-											, @RequestParam(value="day", defaultValue="error") String day
-											, @RequestParam(value="empno", defaultValue="error") String empno
-											, @RequestParam(value="night", defaultValue="error") String night
-											, @RequestParam(value="weekend", defaultValue="error") String weekend
-											, @RequestParam(value="annualAllowance", defaultValue="error") String annualAllowance) {
+	public String pay_Dailywork(Model model, @RequestParam HashMap<String, String> datas) {
 		
-		model.addAttribute("list", idl.callDailyworkInsertDao(year, month, day, empno, night, weekend, annualAllowance));
+		model.addAttribute("list", idl.callDailyworkInsertDao(datas));
 		
 		return "pay/pay_dailyworkList";
+		
+	}
+	
+	@Autowired
+	private InsertTotalCalcList itcl;
+	
+	@RequestMapping("/totalCalculation")
+	public String pay_TotalCalc(Model model, @RequestParam(value="report", defaultValue="0") String report) {
+
+		model.addAttribute("list", itcl.callTotalCalcDao(report));
+		
+		return "pay/pay_salaryCalcList";
+
+	}
+	
+	@Autowired
+	private InsertPersonalCalcList ipcl;
+	
+	@RequestMapping("/personalCalculation")
+	public String pay_PersonalCalc(Model model, @RequestParam(value="report", defaultValue="0") String report
+											  , @RequestParam(value="empno", defaultValue="0") String empno) {
+
+		model.addAttribute("list", ipcl.callPersonalCalcDao(report, empno));
+		
+		return "pay/pay_salaryCalcList";
+
+	}
+	
+	@Autowired
+	private InsertWorkConfirm iwc;
+	
+	@RequestMapping("/insertworkconfirm")
+	public String pay_InsertWorkConfirm(Model model, @RequestParam(value="year", defaultValue="error") String year
+											 	   , @RequestParam(value="month", defaultValue="error") String month
+											 	   , @RequestParam(value="day", defaultValue="error") String day
+											 	   , @RequestParam(value="empno", defaultValue="0") String empno) {
+
+		model.addAttribute("list", iwc.callWorkConfirmDao(year, month, day, empno));
+		
+		return "pay/pay_workconfirmList";
+
+	}
+	
+	@Autowired
+	private SelectWorkConfirm swc;
+	
+	@RequestMapping("/selectworkconfirm")
+	public String pay_SelectWorkConfirm(Model model) {
+
+		model.addAttribute("list", swc.callTotalWorkConfirmDao());
+		
+		return "pay/pay_workconfirmList";
+
+	}
+	
+	@Autowired
+	private UpdatePayConfirmList upcl;
+	
+	@RequestMapping("/payConfirm")
+	public String pay_PayConfirm(Model model, @RequestParam(value="report", defaultValue="0") String report) {
+
+		model.addAttribute("list", upcl.callPayConfirmDao(report));
+		
+		return "pay/pay_salaryCalcList";
+
+	}
+	
+	@Autowired
+	private UpdatePayConfirmCancelList upccl;
+	
+	@RequestMapping("/payConfirmCancel")
+	public String pay_PayConfirmCancel(Model model, @RequestParam(value="report", defaultValue="0") String report) {
+
+		model.addAttribute("list", upccl.callPayConfirmCancelDao(report));
+		
+		return "pay/pay_salaryCalcList";
+
+	}
+	
+	@Autowired
+	private InsertPayrollList ipl;
+	
+	@RequestMapping("/insertPayroll")
+	public String pay_InsertPayroll(Model model, @RequestParam(value="year", defaultValue="0") String year,
+												 @RequestParam(value="month", defaultValue="0") String month,
+												 @RequestParam(value="title", defaultValue="0") String title) {
+		
+		model.addAttribute("list", ipl.callInsertPayrollDao(year, month, title));
+		
+		return "pay/pay_salaryCalcList";
+
+	}
+	
+	@Autowired
+	private DeletePayroll dpi;
+	
+	@RequestMapping("/deletePayroll")
+	public String pay_deletePayroll(Model model, @RequestParam(value="report", defaultValue="error") String report) {
+				
+		model.addAttribute("list", dpi.callDeletePayrollDao(report));
+		
+		return "pay/pay_salaryCalcList";
 		
 	}
 
